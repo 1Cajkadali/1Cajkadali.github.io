298fc09ef9e8314d76af6273298f5b0be1cd1c71
Dalibor Čajka <cajka.dali@gmail.com>
2020-10-29 08:07:00 +0100

    hover

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 const ul = document.getElementsByClassName('list-group');
 const li = document.getElementsByClassName('list-group-item');
 
-function hover(list) {
-    ul.addEventListener('hover', e => {
-        list.style.backgroundColor = 'tomato';
-    })
-};
-
-hover(li);
+ul.addEventListener('hover', e => {
+    e.target.style.color = 'yellow';
+});
