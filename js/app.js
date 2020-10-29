726e7bf87318c72454c1e77bfac771e62c2d2651
Dalibor Čajka <cajka.dali@gmail.com>
2020-10-29 07:46:43 +0100

    JS 2

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const gitContainer = document.querySelector('.git-container')
-const github = document.querySelectorAll('.github');
-
-gitContainer.addEventListener('hover', e => {
-    if (e.target === github) {
-        github.style.backgroundColor = 'tomato';
-    }
-});
